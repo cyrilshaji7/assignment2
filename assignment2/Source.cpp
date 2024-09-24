d0bc62dd6f2fcf8c1046d74eb1ce0018ceb230ba
cyril shaji <cyrilshaji7@gmail.com>
2024-09-23 21:10:41 -0400

    step 4

--- assignment2/Source.cpp
+++ assignment2/Source.cpp
@@ -14,10 +14,12 @@ struct STUDENT_DATA {
 void readStudentData(const string& filename, vector<STUDENT_DATA>& students) {
     ifstream file(filename);
     string line;
+
     if (!file.is_open()) {
         cerr << "Error opening file: " << filename << endl;
         return;
     }
+
     while (getline(file, line)) {
         istringstream ss(line);
         string firstName, lastName;
@@ -29,14 +31,29 @@ void readStudentData(const string& filename, vector<STUDENT_DATA>& students) {
             students.push_back(student);
         }
     }
+
     file.close();
 }
 
+void displayStudents(const vector<STUDENT_DATA>& students) {
+    cout << "Students List:\n";
+    for (auto& s : students) {
+        cout << s.firstName << " " << s.lastName << endl;
+    }
+}
 
 int main() {
     vector<STUDENT_DATA> students;
-
     readStudentData("C:\\Users\\Cyril\\Downloads\\resource-files\\StudentData.txt", students);
+
+#ifdef _DEBUG
+    cout << "Debug Mode On.\n";
+    for (const auto& student : students) {
+        cout << "First Name: " << student.firstName << ", Last Name: " << student.lastName << endl;
+    }
+#endif
+
+    displayStudents(students);
 
     return 1;
 }
