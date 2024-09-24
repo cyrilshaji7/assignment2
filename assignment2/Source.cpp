57f0584cd8533643815d6e9e129694df9abf3a6a
cyril shaji <cyrilshaji7@gmail.com>
2024-09-23 21:05:48 -0400

    step 1

--- assignment2/Source.cpp
+++ assignment2/Source.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
 using namespace std;
 
-int main() {
+struct STUDENT_DATA {
+    string firstName;
+    string lastName;
+};
 
-	return 1;
+void readStudentData(const string& filename, vector<STUDENT_DATA>& students) {
+    ifstream file(filename);
+    string line;
+    if (!file.is_open()) {
+        cerr << "Error opening file: " << filename << endl;
+        return;
+    }
+    while (getline(file, line)) {
+        istringstream ss(line);
+        string firstName, lastName;
+
+        if (getline(ss, firstName, ',') && getline(ss, lastName)) {
+            STUDENT_DATA student;
+            student.firstName = firstName;
+            student.lastName = lastName;
+            students.push_back(student);
+        }
+    }
+    file.close();
+}
+
+
+int main() {
+    vector<STUDENT_DATA> students;
+
+    readStudentData("C:\\Users\\Cyril\\Downloads\\resource-files\\StudentData.txt", students);
+
+    return 1;
 }
